0903aa208f07aef62e36c4d67aa59b38c884f273
Andrew Wright <wrightak@gmail.com>
2022-03-04 14:57:48 +0900

    Dummy, Stub, Spy

--- src/test/java/missile/MissileLauncherTest.java
+++ src/test/java/missile/MissileLauncherTest.java
@@ -2,11 +2,36 @@ package missile;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static missile.MissileLauncher.launchMissile;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MissileLauncherTest {
     @Test
-    void givenExpiredLaunchCodes_MissileIsNotLaunched() {
+    void givenGoodLaunchCodes_MissileIsLaunched() {
+        // arrange
+        MissileSpy missileSpy = new MissileSpy();
 
+        // act
+        launchMissile(missileSpy, new GoodLaunchCodeStub());
+
+        // assert
+        assertThat(missileSpy.launchWasCalled()).isTrue();
+    }
+
+    @Test
+    void givenExpiredLaunchCodes_MissileIsNotLaunched_dummy() {
+        launchMissile(new DummyMissile(), new ExpiredLaunchCodeStub());
+    }
+
+    @Test
+    void givenExpiredLaunchCodes_MissileIsNotLaunched_Spy() {
+        // arrange
+        MissileSpy missileSpy = new MissileSpy();
+
+        // act
+        launchMissile(missileSpy, new ExpiredLaunchCodeStub());
+
+        // assert
+        assertThat(missileSpy.launchWasCalled()).isFalse();
     }
 }
