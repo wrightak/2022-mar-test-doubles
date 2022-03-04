302c6c868feaa1af902d5563f99c17711e5e23c2
Andrew Wright <wrightak@gmail.com>
2022-03-04 11:07:32 +0900

    Initial commit

---
+++ src/test/java/missile/MissileLauncherTest.java
@@ -0,0 +1,12 @@
+package missile;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MissileLauncherTest {
+    @Test
+    void givenExpiredLaunchCodes_MissileIsNotLaunched() {
+
+    }
+}
